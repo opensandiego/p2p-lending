f744d0addd0db90aea2bbc6b2c6d98d0b68e7ba2
Nick Engmann <nengmann@circadence.com>
2019-05-06 14:25:14 -0700

    added babel+react basics

--- p2plending/frontend/src/index.js
+++ p2plending/frontend/src/index.js
@@ -1,18 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const title = 'LENDO';
 
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
 
-var c = 1000;
-function books() {
-    var b = ['📖','📕','📙','📗','📘','📀']
-    if(c <= 0){ return; }else{ c--; }
-    const element = document.createElement('div');
-    element.innerHTML = b[Math.floor(Math.random() * b.length)];
-    element.style.position = "absolute";
-    element.style.fontSize = (Math.random() * 32 + 33).toFixed(0) + "pt";
-    element.style.top = Math.random() * window.innerHeight + "px";
-    element.style.left = Math.random() * window.innerWidth + "px";
-    document.body.appendChild(element);
-    setTimeout(books,20);
-}
-
-books();
+module.hot.accept();
--- p2plending/frontend/webpack.config.js
+++ p2plending/frontend/webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
   }
 };
