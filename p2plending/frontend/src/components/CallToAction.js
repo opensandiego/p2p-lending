fa3a17d586e97cf9f340f0993cc16861b2c7234d
Nick Engmann <cyengmann@gmail.com>
2019-05-14 18:38:14 -0700

    BrowseByLanguage Component with fake backend

---
+++ p2plending/frontend/src/components/CallToAction.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+class CallToAction extends Component {
+    render() {
+      return (
+        <div className="container">
+          <div className="row browseLanguage d-flex align-item-center position-relative pt-2 mx-auto ">
+            <div className="col-xs-6">
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  export default CallToAction;
