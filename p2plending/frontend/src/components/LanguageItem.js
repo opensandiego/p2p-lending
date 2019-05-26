09bf08e75beeb1a8c084a98a945a90d3e7e552dd
Carl Graff <graffcarl@gmail.com>
2019-05-25 17:07:14 -0700

    Add language specific flags and align qty in Browse By Language component

--- p2plending/frontend/src/components/LanguageItem.js
+++ p2plending/frontend/src/components/LanguageItem.js
@@ -9,17 +9,44 @@ class LanguageItem extends Component {
     number: PropTypes.number.isRequired,
   }
 
+  getFlagEmoji(lang) {
+    //const atribs =  'alt={lang} height="20" width="20"';
+    switch(lang) {
+      case 'English':
+        return <img alt={lang} height="20" width="20"
+                src= "https://github.githubassets.com/images/icons/emoji/unicode/1f1ec-1f1e7.png" />
+      case 'Spanish':
+        return <img alt={lang} height="20" width="20"
+                src="https://github.githubassets.com/images/icons/emoji/unicode/1f1ea-1f1f8.png" />     
+      case 'Chinese':
+        return <img alt={lang} height="20" width="20"
+                src="https://github.githubassets.com/images/icons/emoji/unicode/1f1e8-1f1f3.png?" />
+      case 'Korean':
+        return <img alt={lang} height="20" width="20"
+                src="https://github.githubassets.com/images/icons/emoji/unicode/1f1f0-1f1f7.png?" />
+      case 'Japanese':
+      return <img alt={lang} height="20" width="20"
+                src="https://github.githubassets.com/images/icons/emoji/unicode/1f1ef-1f1f5.png?" />   
+      case 'Russian':
+        return <img alt={lang} height="20" width="20"
+                src="https://github.githubassets.com/images/icons/emoji/unicode/1f1f7-1f1fa.png?" />           
+      default:
+        return '⚈';
+    }
+  }
+  
   render() {
     
     return (
       <div className="deck-item col-4 col-sm-3 col-md-4 col-lg-3 d-flex">
-        ⚈ <Link
+        {this.getFlagEmoji(this.props.name)}
+        <Link
           to={`/language/${this.props.name.toString().toLowerCase()}`}
           className="d-flex flex-column justify-content-between text-dark mb-1 p-1 w-50 position-relative"
           style={{ fontSize: "14px" }}
         >
           <div style={{ textDecoration: "underline" }}>
-             {this.props.name}{" "}({ this.props.number })
+             {this.props.name}&nbsp;({ this.props.number })
           </div>
         </Link>
       </div>
