610a31f326fa3abb8eb04560914d9d01d1298b19
Carl Graff <graffcarl@gmail.com>
2019-06-05 11:34:05 -0700

    Fix format of login modal. Change Webpack config  to retain orginal image names & location

--- p2plending/frontend/webpack.config.js
+++ p2plending/frontend/webpack.config.js
@@ -20,7 +20,9 @@ module.exports = {
         use: [
           {
             loader: 'file-loader',
-            options: {},
+            options: {
+              name: '[path][name].[ext]',
+            },
           },
         ],
       }
