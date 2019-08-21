13ce1ea344e1724699b1aff64ec23b72511bfda6
BolunThompson <abolunthompson@gmail.com>
2019-08-21 07:29:27 -0700

    added the rest of the loan admin actions

--- p2plending/lending/admin.py
+++ p2plending/lending/admin.py
@@ -59,7 +59,7 @@ class LoanAdmin(admin.ModelAdmin):
     list_display = ("item","status","due_date","borrower")
     liat_filter = ("status","due_date")
     raw_id_fields = ("borrower","item","renewal_of")
-    actions = ["confirm_lender_dropoff","confirm_borrower_return","confirm_lender_pickup"]
+    actions = ["confirm_lender_dropoff","confirm_borrower_pickup","confirm_lender_return","confirm_lender_pickup","mark_item_lost","record_return_issue"]
 
     def confirm_lender_dropoff(self,request,queryset):
         confirmed = 0
@@ -79,8 +79,25 @@ class LoanAdmin(admin.ModelAdmin):
         for loan in queryset:
             loan.confirm_borrower_return()
             confirmed += 1
-        self.message_user(request,"confirmed %i borrow returns" % confirmed)
-    
+        self.message_user(request,"confirmed %i borrower returns" % confirmed)
+    def confirm_lender_pickup(self,request,queryset):
+        confirmed = 0
+        for loan in queryset:
+            loan.confirm_lender_pickup()
+            confirmed += 1
+        self.message_user(request,"confirmed %i lender pickups" % confirmed)
+    def mark_item_lost(self,request,queryset):
+        confirmed = 0
+        for loan in queryset:
+            loan.mark_item_lost()
+            confirmed += 1
+        self.message_user(request,"marked %i items lost" % confirmed)
+    def record_return_issue(self,request,queryset):
+        confirmed = 0
+        for loan in queryset:
+            loan.record_return_issue()
+            confirmed += 1
+        self.message_user(request,"recored %i issues" % confirmed)
 
 admin.site.register(Loan,LoanAdmin)
 
